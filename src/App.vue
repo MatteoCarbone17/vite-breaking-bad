5009ba7916f607d50f83930587901e4321db6292
MatteoCarbone17 <matteocarbone17@live.com>
2023-01-11 16:19:18 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import AppMain from './components/AppMain.vue'
+
+
+
+export default {
+    components :{
+        AppMain,
+    }
+    
+}
+</script>
+
+
+
+<template>
+
+<AppMain />
+
+</template>
+
+<style lang="scss">
+
+@use './styles/general.scss' as *;
+@use '../node_modules/bootstrap/scss/bootstrap.scss' as *;
+
+</style>
